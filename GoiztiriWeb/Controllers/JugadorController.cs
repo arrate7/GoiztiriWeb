ca40a0c16311983bba5a45fb718714cf14239634
arrate7 <43437355+arrate7@users.noreply.github.com>
2018-11-05 02:10:00 +0100

    .

---
+++ GoiztiriWeb/Controllers/JugadorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GoiztiriWeb.Models;
+
+namespace GoiztiriWeb.Controllers
+{
+    public class JugadorController : Controller
+    {
+        // GET: Jugador
+        public ActionResult Femenino()
+        {
+            //CREAMOS UNA INSTANCIA DEL MODELO JUGADOR
+            var f1 = new Jugador()
+            {
+                Dorsal = 7, Nombre = "Arrate", Apellido = "Ortiz de Zarate", AnioInicio = 2014, Goles = 1, Asistencias = 1
+            };
+            return View(f1);
+        }
+    }
+}
---
+++ GoiztiriWeb/Models/Jugador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoiztiriWeb.Models
+{
+    public class Jugador
+    {
+        public int Dorsal { get; set; }
+        public string Nombre{ get; set; }
+        public string Apellido{ get; set; }
+        public int AnioInicio { get; set; }
+        public int Goles { get; set; }
+        public int Asistencias { get; set; }
+
+    }
+}
